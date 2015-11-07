f052d47345b7c7ff104cabb0e3eb8265e068f4e0
Stefano Cianciulli <stefano@foneclay.com>
2015-11-07 16:13:23 +0100

    Implemented first version of GUI.

---
+++ src/it/cianciustyles/addressbook/Main.java
@@ -0,0 +1,10 @@
+package it.cianciustyles.addressbook;
+
+import it.cianciustyles.addressbook.view.AddressBook;
+
+public class Main {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.setVisible(true);
+    }
+}
---
+++ src/it/cianciustyles/addressbook/view/AddressBook.java
@@ -0,0 +1,12 @@
+package it.cianciustyles.addressbook.view;
+
+import javax.swing.*;
+
+public class AddressBook extends JFrame {
+    public AddressBook() {
+        setTitle("AddressBook by CianciuStyles");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
