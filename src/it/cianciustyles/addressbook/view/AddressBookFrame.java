608888fcacc19bcb25774e089e993aa87b179b68
Stefano Cianciulli <stefano@foneclay.com>
2015-11-08 17:59:11 +0100

    Added the JMenu to the application.

--- src/it/cianciustyles/addressbook/view/AddressBookFrame.java
+++ src/it/cianciustyles/addressbook/view/AddressBookFrame.java
@@ -1,18 +1,65 @@
 package it.cianciustyles.addressbook.view;
 
+import com.sun.codemodel.internal.JOp;
 import it.cianciustyles.addressbook.model.AddressBookTableModel;
 import it.cianciustyles.addressbook.model.Contact;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class AddressBookFrame extends JFrame {
     private AddressBookTableModel tableModel;
     private JTable addressBookTable;
 
+    private ActionListener newContactListener = e -> {
+        NewEditContactFrame newContactFrame = new NewEditContactFrame(tableModel, null, -1);
+        newContactFrame.setVisible(true);
+    };
+
+    private ActionListener editContactListener = e -> {
+        int rowSelected = addressBookTable.getSelectedRow();
+        if (rowSelected == -1) {
+            JOptionPane.showMessageDialog(this, "Please select a contact to be edited!", "Edit Contact...", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        Contact contact = tableModel.getContact(rowSelected);
+        NewEditContactFrame editContactFrame = new NewEditContactFrame(tableModel, contact, addressBookTable.getSelectedRow());
+        editContactFrame.setVisible(true);
+    };
+
+    private ActionListener deleteContactListener = e -> {
+        int rowSelected = addressBookTable.getSelectedRow();
+        if (rowSelected == -1) {
+            JOptionPane.showMessageDialog(this, "Please select a contact to be deleted!", "Delete Contact...", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        Contact selectedContact = tableModel.getContact(rowSelected);
+        String firstName = selectedContact.getFirstName();
+        String lastName = selectedContact.getLastName();
+        int result = JOptionPane.showConfirmDialog(this, String.format("Are you you want to delete %s %s?", firstName, lastName), "Delete Contact...", JOptionPane.YES_NO_OPTION);
+        if (result == JOptionPane.YES_OPTION)
+            tableModel.deleteContact(rowSelected);
+    };
+
+    private ActionListener quitListener = e -> {
+        setVisible(false);
+        dispose();
+        System.exit(0);
+    };
+
+    private ActionListener aboutListener = e -> {
+        JOptionPane.showMessageDialog(this, "AddressBook has been written by Stefano Cianciulli for the InfoSons hiring process.", "About this program", JOptionPane.INFORMATION_MESSAGE);
+    };
+
     public AddressBookFrame() {
         // Initialize the JFrame
-        setTitle("AddressBook by CianciuStyles");
+        setTitle("AddressBook");
         setSize(480, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -26,6 +73,9 @@ public class AddressBookFrame extends JFrame {
 
         // Add the buttons to the JFrame
         addButtons(pane);
+
+        // Add the JMenu to the JFrame
+        addMenu(this);
     }
 
     private void addButtons(Container pane) {
@@ -34,39 +84,13 @@ public class AddressBookFrame extends JFrame {
         controlsContainer.setLayout(flowLayout);
 
         JButton newButton = new JButton("Add new contact...");
-        newButton.addActionListener(e -> {
-            NewEditContactFrame newContactFrame = new NewEditContactFrame(tableModel, null, -1);
-            newContactFrame.setVisible(true);
-        });
+        newButton.addActionListener(newContactListener);
 
         JButton editButton = new JButton("Edit contact...");
-        editButton.addActionListener(e -> {
-            int rowSelected = addressBookTable.getSelectedRow();
-            if (rowSelected == -1) {
-                JOptionPane.showMessageDialog(this, "Please select a contact to be edited!", "Edit Contact...", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            Contact contact = tableModel.getContact(rowSelected);
-            NewEditContactFrame editContactFrame = new NewEditContactFrame(tableModel, contact, addressBookTable.getSelectedRow());
-            editContactFrame.setVisible(true);
-        });
+        editButton.addActionListener(editContactListener);
 
         JButton deleteButton = new JButton("Delete contact");
-        deleteButton.addActionListener(e -> {
-            int rowSelected = addressBookTable.getSelectedRow();
-            if (rowSelected == -1) {
-                JOptionPane.showMessageDialog(this, "Please select a contact to be deleted!", "Delete Contact...", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            Contact selectedContact = tableModel.getContact(rowSelected);
-            String firstName = selectedContact.getFirstName();
-            String lastName = selectedContact.getLastName();
-            int result = JOptionPane.showConfirmDialog(this, String.format("Are you you want to delete %s %s?", firstName, lastName), "Delete Contact...", JOptionPane.YES_NO_OPTION);
-            if (result == JOptionPane.YES_OPTION)
-                tableModel.deleteContact(rowSelected);
-        });
+        deleteButton.addActionListener(deleteContactListener);
 
         controlsContainer.add(newButton);
         controlsContainer.add(editButton);
@@ -86,5 +110,52 @@ public class AddressBookFrame extends JFrame {
         tableContainer.add(scrollPane);
 
         pane.add(tableContainer, BorderLayout.CENTER);
+    }
+
+    private void addMenu(JFrame frame) {
+        JMenuBar menuBar = new JMenuBar();
+
+        // Create the Contacts Menu
+        JMenu contactsMenu = new JMenu("Contacts");
+        contactsMenu.setMnemonic(KeyEvent.VK_C);
+
+        JMenuItem newContact = new JMenuItem("New Contact...");
+        newContact.setMnemonic(KeyEvent.VK_N);
+        newContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+        newContact.addActionListener(newContactListener);
+        contactsMenu.add(newContact);
+
+        JMenuItem editContact = new JMenuItem("Edit Selected Contact...");
+        editContact.setMnemonic(KeyEvent.VK_E);
+        editContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
+        editContact.addActionListener(editContactListener);
+        contactsMenu.add(editContact);
+
+        JMenuItem deleteContact = new JMenuItem("Delete Selected Contact");
+        deleteContact.setMnemonic(KeyEvent.VK_D);
+        deleteContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_DOWN_MASK));
+        deleteContact.addActionListener(deleteContactListener);
+        contactsMenu.add(deleteContact);
+
+        contactsMenu.add(new JSeparator());
+
+        JMenuItem quit = new JMenuItem("Quit");
+        quit.setMnemonic(KeyEvent.VK_Q);
+        quit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
+        quit.addActionListener(quitListener);
+        contactsMenu.add(quit);
+
+        menuBar.add(contactsMenu);
+
+        // Create the ? Menu
+        JMenu aboutMenu = new JMenu("?");
+        JMenuItem aboutItem = new JMenuItem("About...");
+        aboutItem.setMnemonic(KeyEvent.VK_A);
+        aboutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
+        aboutItem.addActionListener(aboutListener);
+        aboutMenu.add(aboutItem);
+        menuBar.add(aboutMenu);
+
+        frame.setJMenuBar(menuBar);
     }
 }
